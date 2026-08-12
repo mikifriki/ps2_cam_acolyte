0372fd0ceb07020fec63ef3a9ea86873d510dd2b
mikifriki <mihkel@miq.ee>
2026-08-12 17:50:36 +0300

    new game

--- ps2_cam_acolyte/games/0FF28F8B_ESPN_Winter_X Games_Snowboarding_2002.cpp
+++ ps2_cam_acolyte/games/0FF28F8B_ESPN_Winter_X Games_Snowboarding_2002.cpp
@@ -9,7 +9,9 @@ class espn_winter_x_games_snowboarding_2002 : public ps2_game
 private:
 	sentinel_counter sentinel;
 	read_only_value_set<float, 3> player_position;
+	read_only_value_set<float, 3> motion_vector;
 	uint32_t player_position_address = 0;
+	uint32_t motion_vector_address = 0;
 	bool player_position_available = false;
 	float position_edit[3] = { 0.0f, 0.0f, 0.0f };
 	bool position_editor_initialized = false;
@@ -20,9 +22,10 @@ private:
 	static constexpr uint32_t player_aggregate = 0x00314710;
 	static constexpr uint32_t physics_pointer_offset = 0x0C;
 	static constexpr uint32_t physics_position_offset = 0x10;
+	static constexpr uint32_t physics_motion_vector_offset = 0x70;
 	static constexpr uint32_t ee_ram_size = 0x02000000;
 
-	bool update_player_position(const pcsx2& ps2)
+	bool update_player_state(const pcsx2& ps2)
 	{
 		ps2_ipc_cmd pointer_read(ps2);
 		auto physics_object = pointer_read.queue_read<uint32_t>(player_aggregate + physics_pointer_offset);
@@ -30,23 +33,28 @@ private:
 		const auto physics_object_address = pointer_read.read(physics_object);
 
 		if (physics_object_address == 0 ||
-			physics_object_address > ee_ram_size - physics_position_offset - (3 * sizeof(float)))
+			physics_object_address > ee_ram_size - physics_motion_vector_offset - (3 * sizeof(float)))
 		{
 			player_position_address = 0;
+			motion_vector_address = 0;
 			player_position_available = false;
 			position_editor_initialized = false;
 			return false;
 		}
 
 		const auto new_position_address = physics_object_address + physics_position_offset;
+		const auto new_motion_vector_address = physics_object_address + physics_motion_vector_offset;
 		if (new_position_address != player_position_address)
 		{
 			player_position_address = new_position_address;
+			motion_vector_address = new_motion_vector_address;
 			player_position.update_base_address(player_position_address);
+			motion_vector.update_base_address(motion_vector_address);
 		}
 		else
 		{
 			player_position.update();
+			motion_vector.update();
 		}
 
 		player_position_available = true;
@@ -65,6 +73,7 @@ public:
 	explicit espn_winter_x_games_snowboarding_2002(const pcsx2& ps2)
 		: sentinel(ps2, 0x01000000)
 		, player_position(ps2, uint32_t{ 0 })
+		, motion_vector(ps2, uint32_t{ 0 })
 	{
 	}
 
@@ -76,6 +85,19 @@ public:
 			ImGui::Text("X: %.3f", player_position.get(position_x));
 			ImGui::Text("Y: %.3f", player_position.get(position_y));
 			ImGui::Text("Z: %.3f", player_position.get(position_z));
+
+			const auto motion_x = motion_vector.get(position_x);
+			const auto motion_y = motion_vector.get(position_y);
+			const auto motion_z = motion_vector.get(position_z);
+			const auto motion_magnitude = std::sqrt(
+				(motion_x * motion_x) + (motion_y * motion_y) + (motion_z * motion_z));
+
+			ImGui::Separator();
+			ImGui::Text("Rider motion vector");
+			ImGui::Text("X: %+.3f", motion_x);
+			ImGui::Text("Y: %+.3f", motion_y);
+			ImGui::Text("Z: %+.3f", motion_z);
+			ImGui::Text("Magnitude: %.3f", motion_magnitude);
 		}
 		else
 		{
@@ -119,11 +141,12 @@ public:
 		if (sentinel.has_reset())
 		{
 			player_position_address = 0;
+			motion_vector_address = 0;
 			player_position_available = false;
 			position_editor_initialized = false;
 		}
 
-		update_player_position(ps2);
+		update_player_state(ps2);
 	}
 
 	bool needs_ui_refresh() const override
